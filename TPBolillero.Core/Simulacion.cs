431fc6e8f6160a1485df8822998ffc83eda372d6
NicolasBenitezs30 <nicolasbenitez1438@gmail.com>
2022-05-26 15:41:20 -0300

    Update Simulacion.cs

--- TPBolillero.Core/Simulacion.cs
+++ TPBolillero.Core/Simulacion.cs
@@ -10,13 +10,14 @@ namespace TPBolillero.Core
         public long SimularSinHilos(Bolillero bolillero, List<byte> numeros, long veces)
             => bolillero.JugarN(numeros, veces);
 
-        public long SimularConHilos(Bolillero bolillero, List<byte> numeros, long veces, long hilos)
+        public long SimularConHilos(Bolillero bolillero, List<byte> numeros, long veces, int hilos)
         {
             Task<long>[] tareas = new Task<long>[hilos];
-            for(long i = 0; i < hilos; i++)
+            for(int i = 0; i < hilos; i++)
             {
                 var clon = (Bolillero)bolillero.Clone();
-                Task uno = Task.Run( () => clon );
+                Task tareas[i] = Task.Run( () => SimularSinHilos(clon, numeros, veces/hilos));
+                
 
             }
             
